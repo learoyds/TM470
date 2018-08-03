af883893063075c622456e048e551ea5700a1867
Chris Learoyd <learoyds@hotmail.com>
2018-08-03 09:02:26 +0100

    Changed Allergies
    Altered links to neutralise

--- allergies.php
+++ allergies.php
@@ -13,13 +13,13 @@
     <center><h2>Let us create you a bespoke web presence.</h2>
     <h3>For a look at some examples click on the links below.</h3>
     <span class="red">
-    <a href="http://www.harrogatemarketplace.co.uk" target="_blank" title="Buy and sell in Harrogate">Harrogate Marketplace</a>
-    <a href="Renewable planet/index.html" target="_blank" title="Complicated site showing many different possibilities">Site using JQuery CSS</a>
-    <a href="KoalaBox.html" title="Corousel of pictures">Javascript carousel</a>
-    <a href="HTMLLetter/index.htm" target="_blank" title="Advertise to your email lists">HTML email example</a>
-    <a href="resources/OOo.swf" target="_blank" title="Tutorials from Learoyds Computing">Flash tutorial example</a>
-    <a href="examples/Droppy and Carousel.html" target="_blank" title="A basic header with a mix of javascript and css">Carousel and basic menu</a>
-    <a href="examples/Multi Dropdown Navbar.html" target="_blank" title="CSS dropdown menu">CSS menu</a>
+    <a href="#" target="_blank" title="Gluten intollerence">Coeliac</a>
+    <a href="#" target="_blank" title="Religeous restrictions in diet">Religeon</a>
+    <a href="#" title="Lactose intollerence">Milk products</a>
+    <a href="#" target="_blank" title="a">a</a>
+    <a href="#" target="_blank" title="b">b</a>
+    <a href="#" target="_blank" title="c">c</a>
+    <a href="#" target="_blank" title="d">d</a>
     </span><br />
     Watch this space... more to come!
     </center>	
