92f05b9162e38b6fd89cad31703713daa631d06b
Chris Learoyd <learoyd@hotmail.com>
2018-08-11 19:27:24 +0100

    trying to fix vs

--- includes/HEAD2_Include.php
+++ includes/HEAD2_Include.php
@@ -1,8 +1,8 @@
 <?php
 echo '<script src="JS/JScript.js" language="JavaScript" type="text/javascript"></script>
-      <script language="javascript" type="text/javascript">
-        screenSize();
-      </script>
+      <!--<script language="javascript" type="text/javascript">
+         screenSize();
+      </script>*/ -->
 
  
 </head>
